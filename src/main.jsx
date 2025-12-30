f16efa82c359f90992d2c70ebd0a8394925147a1
Mr.Sima <mrsima7id@outlook.com>
2025-12-31 04:43:50 +0530

    Add files via upload

---
+++ src/main.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import "./index.css";
+
+
+sessionStorage.clear();
+localStorage.clear();
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
